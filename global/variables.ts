ff4c8c2fd88f6b67b5ba2fb532625af90952601a
Mario Krstevski <mariokrstevski@hotmail.com>
2024-01-11 01:18:19 +0100

    dame config variables

--- global/variables.ts
+++ global/variables.ts
@@ -1,5 +1,5 @@
 const defaultStoreId = "8aacb4bd-6b25-4500-aa2f-71eaa77e9abe"; // Suzana Kroevi
 const defaultBillboardId = "8324be14-61c1-4281-b96e-2045e56c84cf"; // Vencanici
 
-export const storeId = "d87ce47d-8841-479d-87f6-a39789c888c2"; //Cat Store
-export const billboardId = "ba07658c-2247-4e9e-8144-ed89a3b2a81a"; //Puff
+export const storeId = "f2643fee-d534-4795-afa1-968683c572f8"; // Vegas jackpot
+export const billboardId = "e3a88483-145c-4a98-aa5c-c35c7eb2ccc3"; //da
