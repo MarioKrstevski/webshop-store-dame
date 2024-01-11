e774592436f37f38ecf196884ec9b0cb6b47393d
Mario Krstevski <mariokrstevski@hotmail.com>
2024-01-11 01:17:08 +0100

    dame deploy

---
+++ global/variables.ts
@@ -0,0 +1,5 @@
+const defaultStoreId = "8aacb4bd-6b25-4500-aa2f-71eaa77e9abe"; // Suzana Kroevi
+const defaultBillboardId = "8324be14-61c1-4281-b96e-2045e56c84cf"; // Vencanici
+
+export const storeId = "d87ce47d-8841-479d-87f6-a39789c888c2"; //Cat Store
+export const billboardId = "ba07658c-2247-4e9e-8144-ed89a3b2a81a"; //Puff
